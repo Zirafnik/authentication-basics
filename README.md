eee6ae531cf5996cecc2557685be533e1d7f232f
david <dh@ptica.com>
2021-12-08 22:38:29 +0100

    added authentication and login

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple website where I practice authentication basics with `passport.js` and `express-session`.
+
+Users are able to sign-up using *username* and *password*.
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -23,11 +24,64 @@ const app = express();
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({username: username})
+            .then(user => {
+                if(!user) {
+                    return done(null, false, {message: 'Incorrect username'});
+                }
+
+                if(user.password !== password) {
+                    return done(null, false, {message: 'Incorrect password'});
+                }
+
+                return done(null, user);
+            })
+            .catch(err => done(err));
+    })
+);
+
+passport.serializeUser(function(user, done) {
+    //user.id is a getter for a hex string of _.id ObjectID(string) in Mongoose;
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
 app.use(session({secret: 'cats', resave: false, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res, next) => res.render('index', {user: req.user}));
+app.get('/sign-up', (req, res, next) => res.render('sign-up-form', {errorMsg: false}));
+
+app.post('/sign-up', async (req, res, next) => {
+    let userCheck = await User.findOne({username: req.body.username});
+    if(userCheck) {
+        //return the response so the function finishes
+        return res.render('sign-up-form', {errorMsg: 'Username already taken'});
+    }
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .save()
+    .then(success => res.redirect('/'))
+    .catch(err => next(err));
+});
+
+app.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/'}));
+
+app.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/');
+    return;
+})
 
 app.listen(3000, () => console.log('app listening on port: 3000'));
